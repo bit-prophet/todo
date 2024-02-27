90907d4d24b2c145f358341786207754f66109cf
mohsen <mohsen.nouri1372@gmail.com>
2024-02-28 03:18:58 +0330

    use fillable instead of guarded in the todo model

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -10,7 +10,7 @@ class Todo extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id', 'created_at', 'updated_at'];
+    protected $fillable = ['user_id', 'title', 'completed'];
 
     public function user(): BelongsTo
     {
